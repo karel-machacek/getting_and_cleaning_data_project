137de34e4b5c7fde50a969a0e895e57c427079fc
Machacek <machakar@CZPRCOMACHAKAR3.local>
2015-11-19 19:33:14 +0100

    run_analysis.R file created

---
+++ run_analysis.R
@@ -0,0 +1,11 @@
+install.packages("plyr")
+library(plyr)
+
+setwd("/Users/machakar/Documents/private/private_karel/karel/Coursera/getting_and_cleaning_data/PROJ")
+fileURL="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL,"dataset.zip")
+unzip("dataset.zip")
+setwd("UCI\ HAR\ Dataset")
+unzip
+merge
+rename
